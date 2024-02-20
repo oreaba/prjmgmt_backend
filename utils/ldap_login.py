cc75db1a734f42647cd1f9a711adad0398b7a7ba
Mohammad Hamdy Ibrahim Oreaba <Mohammad.Oreaba@adm.gov.ae>
2024-02-20 09:15:02 +0400

    LDAP module

--- utils/ldap_login.py
+++ utils/ldap_login.py
@@ -1,11 +1,12 @@
 from ldap3 import Server, Connection
 from ldap3.core.exceptions import LDAPSocketOpenError, LDAPBindError
+import getpass
 def login():
     domain = 'adm.local' 
     server = Server(f'ldap://{domain}')
     username = input("Enter username: ")
     username += '@adm.gov.ae'
-    password = input("Enter password: ")
+    password = getpass.getpass("Enter password: ")
     try:
         with Connection(server, user=username, password=password, auto_bind=True) as conn:
             print("welcome ", conn.extend.standard.who_am_i())
