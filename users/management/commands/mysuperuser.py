987921dda93fe2b5907a7f2c7e284fb6ec36fd87
Mohammad Hamdy Ibrahim Oreaba <Mohammad.Oreaba@adm.gov.ae>
2024-03-05 15:09:34 +0400

    deploy

--- users/management/commands/mysuperuser.py
+++ users/management/commands/mysuperuser.py
@@ -1,12 +1,3 @@
-# import os
-# from django.core.management.base import BaseCommand
-# from django.contrib.auth.models import User
-
-# class Command(BaseCommand):
-#     def handle(self, *args, **options):
-#         
-
-import os
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 
