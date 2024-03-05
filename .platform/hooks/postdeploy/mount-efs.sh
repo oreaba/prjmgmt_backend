a1408ff1773e02b2c5732ccb6d380cad67823103
Mohammad Hamdy Ibrahim Oreaba <Mohammad.Oreaba@adm.gov.ae>
2024-03-05 14:59:32 +0400

    deploy

--- .platform/hooks/postdeploy/mount-efs.sh
+++ .platform/hooks/postdeploy/mount-efs.sh
@@ -55,6 +55,7 @@ fi
 echo "Applyging migrations and creating a new superuser"
 chmod 777 ${EFS_MOUNT_DIR}/adm-pm.db.sqlite3
 source /var/app/venv/*/bin/activate
+cd /var/app/current/
 python manage.py makemigrations && python manage.py migrate
 python manage.py mysuperuser
 echo 'EFS mount complete.'
