b12dd89c0e17501d15d55306fc0d9ee91bb7d9bc
Mohammad Hamdy Ibrahim Oreaba <Mohammad.Oreaba@adm.gov.ae>
2024-03-05 15:15:46 +0400

    deploy

--- projects/models.py
+++ projects/models.py
@@ -87,7 +87,7 @@ class ProjectStatus(models.Model):
     status_id = models.AutoField(primary_key=True)
     status = models.CharField(max_length=50)
     status_ar = models.CharField(max_length=50, blank=True, null=True)
-    color = models.CharField(max_length=7)  # Assuming color code in hexadecimal format
+    # color = models.CharField(max_length=7)  # Assuming color code in hexadecimal format
     class Meta:
         verbose_name        = "Project Status"
         verbose_name_plural = "Project Status"
@@ -99,7 +99,7 @@ class ProjectPriority(models.Model):
     priority_id = models.AutoField(primary_key=True)
     priority = models.CharField(max_length=50)
     priority_ar = models.CharField(max_length=50, blank=True, null=True)
-    # color = models.CharField(max_length=7)  # Assuming color code in hexadecimal format
+    color = models.CharField(max_length=7)  # Assuming color code in hexadecimal format
     class Meta:
         verbose_name        = "Project Priority"
         verbose_name_plural = "Project Priority"
