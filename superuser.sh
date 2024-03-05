cef03c6c272c949151685ff0e666a26b536f7c48
Mohammad Hamdy Ibrahim Oreaba <Mohammad.Oreaba@adm.gov.ae>
2024-03-05 13:52:17 +0400

    deploy

--- superuser.sh
+++ superuser.sh
@@ -1,4 +1,6 @@
-chmod 775 /efs-adm-pm-db-prod/adm-pm.db.sqlite3
+sudo chmod 777 /efs-adm-pm-db-prod/
+sudo chmod 777 /efs-adm-pm-db-prod/adm-pm.db.sqlite3
+
 source /var/app/venv/staging-*/bin/activate
 cd /var/app/current/
 python manage.py createsuperuser
