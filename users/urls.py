739bbbf1b25ceebf22ae1c5204da0379344d6ac2
Mohammad Hamdy Ibrahim Oreaba <Mohammad.Oreaba@adm.gov.ae>
2024-03-06 12:09:56 +0400

    deploy

--- users/urls.py
+++ users/urls.py
@@ -10,3 +10,20 @@ urlpatterns = [
     path('api/login', LoginView.as_view(), name='api-login'),
    
 ]
+
+
+# # Custom title for your API documentation
+# api_info = openapi.Info(
+#     title="Your Custom API Title",
+#     default_version='v1',
+#     description="Your API description",
+#     terms_of_service="https://www.example.com/policies/terms/",
+#     contact=openapi.Contact(email="contact@example.com"),
+#     license=openapi.License(name="BSD License"),
+# )
+
+# schema_view = get_schema_view(
+#     api_info,
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
