1a59d6cdd5976d420bb2f4d0de1a027d6ccff794
Mohammad Hamdy Ibrahim Oreaba <Mohammad.Oreaba@adm.gov.ae>
2024-03-07 12:17:22 +0400

    deploy

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import Role, RoleDuty
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
+from users.models import PMUser as User
 
 # Register your models here.
 
-# admin.site.register(User)
+admin.site.register(User)
 admin.site.register(Role)
 admin.site.register(RoleDuty)
