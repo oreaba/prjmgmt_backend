c456269032920483425fe60a985fec369d186f86
Mohammad Hamdy Ibrahim Oreaba <Mohammad.Oreaba@adm.gov.ae>
2024-03-18 10:10:08 +0400

    deploy

--- users/admin.py
+++ users/admin.py
@@ -3,18 +3,34 @@ from .models import Role, RoleDuty
 from django.contrib.auth.models import User
 from users.models import PMUser #as User
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from django.forms import inlineformset_factory
 from users.forms import RoleDutyForm
 
 # Register your models here.
+class PMUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = PMUser
+
+class PMUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = PMUser
+
 class PMUserAdmin(UserAdmin):
+    add_form = PMUserCreationForm
+    form = PMUserChangeForm
+    model = PMUser
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff']
+
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
+        (None, {'fields': ('username', 'password' )}),
+        # ('Change Password', {'fields': ('password1','password2')}),
         ('Personal Info', {'fields': ('first_name', 'last_name', 'email')}),
         ('Additional Info', {'fields': ('first_name_ar', 'last_name_ar', 'title', 'title_ar', 'extension', 'mobile')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
+                                        # 'password2'
     )
     add_fieldsets = (
         (None, {
